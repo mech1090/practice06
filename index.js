60cd12e586015ace54784d320453f87c7869860e
Shahzad <mech1090@gmail.com>
2022-06-04 09:57:49 -0600

    main page setup

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('config')
+
+const app = express()
+
+app.use(express())
+app.use(cors())
+app.use(express.json())
+app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
+
+app.get('/',(req,res)=>{
+    res.send('OOK')
+})
+
+app.get('*',(req,res)=>{
+    res.send('BAD_REQUEST')
+})
+
+port = config.get('port') || 8080
+
+app.listen(port,()=>{
+    console.log(`server running on port ${port}`)
+})
